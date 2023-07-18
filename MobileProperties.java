ff599cada4154fc6e1644785b5fb253d905933a2
Monisha07122001 <139787413+Monisha07122001@users.noreply.github.com>
2023-07-18 22:11:58 +0530

    Add files via upload

---
+++ MobileProperties.java
@@ -0,0 +1,25 @@
+package week1.day2.program2;
+
+public class MobileProperties {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String mobileBrandName="oopo";
+		char mobileLogo='@';
+		short noOfMobilePiece=3;
+		int modelNumber=1802;
+		long mobileImeiNumber=2314556578L;
+		float mobilePrice=12.36f;
+		boolean isDamaged=false;
+		
+		
+		System.out.println("Mobile Brand Name :" +mobileBrandName);
+		System.out.println("MobileLogo : " +mobileLogo);
+		System.out.println("No Of Mobile Piece : " +noOfMobilePiece);
+		System.out.println("Model Number : "+modelNumber);
+		System.out.println("Mobile Imei Number : "+mobileImeiNumber);
+		System.out.println("Mobile Price : "+mobilePrice);
+		System.out.println("Is Damaged : " +isDamaged);
+	}
+
+}
