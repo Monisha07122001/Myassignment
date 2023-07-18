75251ade10f0e8528b3a2ed3fdcc2489d6e03eca
Monisha07122001 <139787413+Monisha07122001@users.noreply.github.com>
2023-07-18 22:11:30 +0530

    Add files via upload

---
+++ TwoWheeler.java
@@ -0,0 +1,24 @@
+package week1.day2.program1;
+
+public class TwoWheeler {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int noOfWheels=2;
+		System.out.println("No of Wheels : " +noOfWheels);
+		short noOfMirrors=2;
+		System.out.println("No of Mirors : " +noOfMirrors);
+		long chassisNumber=7123456789L;
+		System.out.println("Chassis Number : " +chassisNumber);
+		boolean isPuntured=true;
+		System.out.println("Is Punctured : " +isPuntured);
+		String bikeName="Honda";
+		System.out.println("Bike Name : " +bikeName);
+		double runningKM=12133545844.1455554458666555;
+		System.out.println("Running KM" +runningKM);
+		
+		
+
+	}
+
+}
